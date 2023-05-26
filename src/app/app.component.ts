487effa47d056c118470606bb7dc2317c0553af1
TitanSarim <Muhammadsarim203@gmail.com>
2023-05-26 23:53:09 +0500

    EXPRESSIONS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+
+
+// * use template or templateUrl at one time
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+selector: 'app-root',
+// template: `
+//   <p>Hello</p>
+// `,
+templateUrl: './app.component.html',
+styleUrls: ['./app.component.css'],
+styles: ['']
 })
-export class AppComponent {
-  title = '1_basics';
+export class AppComponent{
+
+  name = 'Sarim'
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from'@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+
+  declarations:[
+    AppComponent
+  ],
+
   bootstrap: [AppComponent]
+
 })
-export class AppModule { }
+
+export class AppModule{
+
+}
