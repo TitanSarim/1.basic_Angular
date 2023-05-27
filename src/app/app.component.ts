3e02ff13944703a014f25214d3a9172945e2270a
TitanSarim <Muhammadsarim203@gmail.com>
2023-05-27 21:10:19 +0500

    Pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,8 +14,26 @@ styles: ['']
 })
 export class AppComponent{
 
-  name = 'Sarim'
+  name = 'Sarim xahid'
   imgURL = "https://images.unsplash.com/photo-1666919643134-d97687c1826c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1171&q=80"
+
+  images = [
+    "https://images.unsplash.com/photo-1666919643134-d97687c1826c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1171&q=80",
+    "https://images.unsplash.com/photo-1666919643134-d97687c1826c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1171&q=80",
+    "https://images.unsplash.com/photo-1666919643134-d97687c1826c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1171&q=80"
+  ]
+
+  currentDate = new Date()
+  cost = 2000
+  temperature= 25.3
+
+  pizza= {
+    toppings: ['nawabi', 'tikka'],
+    size: 'Large'
+  }
+
+  blueClass = false
+  fontSize = 16
 
   getName(){
     return this.name
